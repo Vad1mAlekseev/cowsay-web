5640bcb791f9025a401433cc5dcd10e8580d615f
Vad1mAlekseev <Vad1mAlekseev2807@gmail.com>
2020-02-05 10:22:41 +0300

    migrate to koa

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
 const { exec } = require('child_process');
-const http = require('http');
+const Koa = require('koa');
+const app = new Koa();
 fs = require('fs');
 const { promisify } = require('util');
 
 global._ = require('lodash');
-const { APP_PORT, APP_HOST } = require('./app-config')
+const { APP_PORT, APP_HOST } = require('./app-config');
 const readFile = promisify(fs.readFile);
 
-const server = http.createServer(async (req, res) => {
-    res.statusCode = 200;
+app.use(async ctx => {
+  const URL = ctx.request.url;
 
-    if (!req.url.slice(1)) {
-        let template = (await readFile('templates/index.html')).toString();
-        res.end(await new Promise((resolve, reject) => {
-            exec(`cowsay -l`, (error, stdout, stderr) => {
-                if (stderr || error) {
-                    res.end(`exec error: ${error}`);
-                    resolve(stderr);
-                    re2turn;
-                }
-                const buttons =
-                    _(stdout)
-                        .split('\n')
-                        .splice(1)
-                        .words()
-                        .value();
-                resolve(_.template(template)({ buttons }));
-            });
-        }));
-        return;
-    }
+  if (!URL.slice(1)) {
+    let template = await readFile('templates/index.html');
+    ctx.body = await new Promise((resolve, reject) => {
+      exec(`cowsay -l`, (error, stdout, stderr) => {
+        if (stderr || error) {
+          ctx.body = `exec error: ${error}`;
+          resolve(stderr);
+          re2turn;
+        }
+        const buttons = _(stdout)
+          .split('\n')
+          .splice(1)
+          .words()
+          .value();
+        resolve(_.template(template)({ buttons }));
+      });
+    });
+    return;
+  }
 
-    res.end(await new Promise((resolve, reject) => {
-        exec(`cowsay -f ${req.url.split('').splice(1).join('')} hello`, (error, stdout, stderr) => {
-            if (stderr || error) {
-                res.end(error.toString());
-                resolve(stderr);
-                return;
-            }
-            resolve(stdout);
-        });
-    }));
+  ctx.body = await new Promise((resolve, reject) => {
+    exec(
+      `cowsay -f ${URL.split('')
+        .splice(1)
+        .join('')} hello`,
+      (error, stdout, stderr) => {
+        if (stderr || error) {
+          ctx.body = error.toString();
+          resolve(stderr);
+          return;
+        }
+        resolve(stdout);
+      }
+    );
+  });
 });
 
-server.listen(APP_PORT, APP_HOST, () => {
-    console.log(`Server listen ${APP_HOST}:${APP_PORT}`);
+app.listen(APP_PORT, APP_HOST, () => {
+  console.log(`Server listen ${APP_HOST}:${APP_PORT}`);
 });
