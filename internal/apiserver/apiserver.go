d632f71b810e555d2df0f7e93fe1cddac1655447
Vad1mAlekseev <Vad1mAlekseev2807@gmail.com>
2021-04-21 23:47:44 +0300

    Add grafana

--- internal/apiserver/apiserver.go
+++ internal/apiserver/apiserver.go
@@ -6,12 +6,21 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 	"github.com/vad1malekseev/cowsay-web/internal/cowsay"
 )
+
+type Metrics struct {
+	totalRequests  *prometheus.CounterVec
+	responseStatus *prometheus.CounterVec
+	httpDuration   *prometheus.HistogramVec
+}
 
 // The Cowsay CLI methods.
 type Cowsay interface {
@@ -21,9 +30,10 @@ type Cowsay interface {
 
 // The APIServer struct for serving cowsay-web.
 type APIServer struct {
-	server *http.Server
-	config *Config
-	logger *logrus.Logger
+	server  *http.Server
+	config  *Config
+	logger  *logrus.Logger
+	metrics *Metrics
 
 	cowsay Cowsay
 }
@@ -31,10 +41,27 @@ type APIServer struct {
 // New creates a APIServer instance providing config.
 func New(cfg *Config) *APIServer {
 	return &APIServer{
-		server: nil,
 		config: cfg,
 		logger: logrus.New(),
-		cowsay: nil,
+		metrics: &Metrics{
+			totalRequests: prometheus.NewCounterVec(
+				prometheus.CounterOpts{
+					Name: "http_requests_total",
+					Help: "Number of get requests.",
+				},
+				[]string{"path"}),
+			responseStatus: prometheus.NewCounterVec(
+				prometheus.CounterOpts{
+					Name: "response_status",
+					Help: "Status of HTTP response",
+				},
+				[]string{"status"},
+			),
+			httpDuration: promauto.NewHistogramVec(prometheus.HistogramOpts{
+				Name: "http_response_time_seconds",
+				Help: "Duration of HTTP requests.",
+			}, []string{"path"}),
+		},
 	}
 }
 
@@ -73,6 +100,12 @@ func (s *APIServer) WaitWithShutdown() {
 
 func (s *APIServer) configureServer() {
 	r := mux.NewRouter()
+
+	_ = prometheus.Register(s.metrics.totalRequests)
+	_ = prometheus.Register(s.metrics.responseStatus)
+	_ = prometheus.Register(s.metrics.httpDuration)
+	r.Use(s.prometheusMiddleware)
+
 	r.HandleFunc("/", s.HomeHandler)
 	r.Handle("/metrics", promhttp.Handler())
 	r.HandleFunc("/favicon.ico", s.FaviconHandler)
@@ -106,4 +139,36 @@ func (s *APIServer) configureLogger() error {
 
 func (s *APIServer) configureCowsay() {
 	s.cowsay = &cowsay.Cowsay{}
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func (s *APIServer) prometheusMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		route := mux.CurrentRoute(r)
+		path, _ := route.GetPathTemplate()
+
+		timer := prometheus.NewTimer(s.metrics.httpDuration.WithLabelValues(path))
+		rw := NewResponseWriter(w)
+		next.ServeHTTP(rw, r)
+
+		statusCode := rw.statusCode
+
+		s.metrics.responseStatus.WithLabelValues(strconv.Itoa(statusCode)).Inc()
+		s.metrics.totalRequests.WithLabelValues(path).Inc()
+
+		timer.ObserveDuration()
+	})
 }
